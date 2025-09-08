fa7ec67148fc99076ddbeb71e6e08e785bfa51a7
Luis Nieto <lnieto@herrera.unt.edu.ar>
2025-09-08 18:50:10 -0300

    Agregar ControladorPrincipal.java

---
+++ src/principal/controladores/ControladorPrincipal.java
@@ -0,0 +1,11 @@
+package principal.controladores;
+
+import productos.modelos.Producto;
+
+
+public class ControladorPrincipal {
+    public static void main(String[] args) {
+        Producto p = new Producto();
+        p.mostrar();
+    }
+}
