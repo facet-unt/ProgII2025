4aacc57ecad4ba580d2dc7ccc1d47a1d12b73659
Tobías Ismael Manzilla <tobiaskpo.1450@gmail.com>
2025-09-09 18:15:51 -0300

    Agregado de la clase Encargado.java

---
+++ src/usuarios/modelos/Encargado.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package usuarios.modelos;
+
+/**
+ *
+ * @author estudiante
+ */
+public class Encargado {
+    String correo;
+    String clave;
+    String nombre;
+    String apellido;
+     void mostrar()
+    {
+        System.out.println("Correo : " + correo);
+        System.out.println("\n");
+        System.out.println("Clave : " + clave);
+        System.out.println("\n");
+        System.out.println("Nombre : " + nombre);
+        System.out.println("\n");
+        System.out.println("Apellido : " + apellido);
+    }
+}
