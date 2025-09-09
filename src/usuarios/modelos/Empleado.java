827e9375d42ce482fb787291e9b85ad1b53e146e
Gaston Romano <romanogaston13@gmail.com>
2025-09-09 18:02:52 -0300

    creo clases (Cliente-Empleado-Encargado) con sus atributos (correo-clave-apellido-nombre)

---
+++ src/usuarios/modelos/Empleado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package usuarios.modelos;
+
+/**
+ *
+ * @author estudiante
+ */
+public class Empleado {
+    String correo;
+    String clave;
+    String apellido;
+    String nombre;
+}
