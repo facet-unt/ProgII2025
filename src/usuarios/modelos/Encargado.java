51b7443b3039ce83faffe5bafe3fd1dd3c576dc3
Esteban Lobo <silvanacho04@gmail.com>
2025-09-09 15:43:39 -0300

    Agregado de las clases: Cliente, Empleado y Encargado

---
+++ src/usuarios/modelos/Encargado.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package usuarios.modelos;
+
+/**
+ *
+ * @author Esteban
+ */
+public class Encargado {
+    //atributos
+    
+    public String correo;
+    public String clave;
+    public String apellido;
+    public String nombre;
+    
+    //metodos
+    
+    void mostrar(){
+        System.out.println("Encargado");
+        
+    }
+}
