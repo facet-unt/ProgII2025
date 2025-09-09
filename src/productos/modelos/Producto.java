08620cabf39520d90bec087f35e8ecae98997a4c
Thomas Diaz <thomasale354@gmail.com>
2025-09-09 18:33:49 -0300

    Commit final de la clase 09/09, TP2 finalizado

--- src/productos/modelos/Producto.java
+++ src/productos/modelos/Producto.java
@@ -9,23 +9,20 @@ package productos.modelos;
  * @author Esteban
  */
 public class Producto {
-    //atributos
-    
+    //Atributos
     public int codigo;
     public String descripcion;
     public String categoria;
     public String estado;
     public float precio;
     
-    //metodos
-    
+    //Metodos
     public void mostrar(){
         System.out.println("cod.: " + codigo + " Descripcion: "+ descripcion);
     }
 
     @Override
     public String toString() {
-        return "Producto{" + "codigo=" + codigo + ", descripcion=" + descripcion + ", categoria=" + categoria + ", estado=" + estado + ", precio= $" + precio + '}';
+        return "Producto{" + "codigo=" + codigo + ", descripcion=" + descripcion + ", categoria=" + categoria + ", estado=" + estado + ", precio=" + precio + '}';
     }
-    
 }
