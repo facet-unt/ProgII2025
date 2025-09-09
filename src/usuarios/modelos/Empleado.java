313c9b11c8e0a7ae5dbb1009273c8276913ac08d
Agustin Arborio <agusarborio@gmail.com>
2025-09-09 17:54:53 -0300

    Creacion de las clases Cliente, Empleado y Encargado

---
+++ src/usuarios/modelos/Empleado.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package usuarios.modelos;
+
+/**
+ *
+ * @author Asus
+ */
+public class Empleado {
+    String correo;
+    String clave;
+    String apellido;
+    String nombre;
+    void mostrar(){
+        System.out.println (correo);
+        System.out.println (clave);
+        System.out.println (apellido);
+        System.out.println (nombre);
+    };
+}
