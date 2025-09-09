13820866afa265733cbed73ffab8333aed6a74fe
msanchez <msanchez>
2025-09-09 16:24:22 -0300

    Tp2 parte1

--- src/principal/controladores/ControladorPrincipal.java
+++ src/principal/controladores/ControladorPrincipal.java
@@ -6,6 +6,11 @@ import productos.modelos.Producto;
 public class ControladorPrincipal {
     public static void main(String[] args) {
         Producto p = new Producto();
+        p.codigo=25;
+        p.descripcion="pizza";
+        
         p.mostrar();
+        System.out.println(p);
+        
     }
 }
--- src/productos/modelos/Producto.java
+++ src/productos/modelos/Producto.java
@@ -2,7 +2,23 @@ package productos.modelos;
 
 
 public class Producto {
+    
+    public int codigo;
+    public String descripcion;
+    public String categoria;
+    public String estado;
+    public float precio;
+    
+    
     public void mostrar() {
         System.out.println("Soy un producto");
+        //IMPLEMNETO LO QUE QUIERO QUE HAGA
     }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "codigo=" + codigo + ", descripcion=" + descripcion + ", categoria=" + categoria + ", estado=" + estado + ", precio=" + precio + '}';
+    }
+    
+    
 }
