7ee0393fe7da1a6c12158392a84edd120ed3e9b5
Tobías Ismael Manzilla <tobiaskpo.1450@gmail.com>
2025-09-09 19:10:40 -0300

    Agregado de ControladorPrincipal.java y Producto.java pero esta incompleto

--- src/principal/controladores/ControladorPrincipal.java
+++ src/principal/controladores/ControladorPrincipal.java
@@ -1,11 +1,27 @@
 package principal.controladores;
 
+import java.util.ArrayList;
 import productos.modelos.Producto;
+import usuarios.modelos.Empleado;
+import usuarios.modelos.Encargado;
 
 
-public class ControladorPrincipal {
-    public static void main(String[] args) {
+public class ControladorPrincipal 
+{
+    public static void main(String[] args) 
+    {
         Producto p = new Producto();
-        p.mostrar();
+        Encargado encargado = new Encargado();
+        Empleado emp = new Empleado();
+        
+        ArrayList<Producto> prod = new ArrayList<>();
+        ArrayList<Encargado> enc = new ArrayList<>();
+        ArrayList<Empleado> empleado = new ArrayList<>();
+        
+        prod.add(new Prodcuto(""))
+        p.adds.descripcion()
+                
+        enc
+                       
     }
 }
--- src/productos/modelos/Producto.java
+++ src/productos/modelos/Producto.java
@@ -1,8 +1,31 @@
 package productos.modelos;
 
 
-public class Producto {
-    public void mostrar() {
-        System.out.println("Soy un producto");
+public class Producto 
+{
+    int codigo;
+    String descripcion;
+    String categoria;
+    String estado;
+    float precio;
+    void mostrar()
+    {
+        System.out.println("------ DATOS DEL PRODUCTO ------");
+        System.out.println("Descripcion : " + descripcion);
+        System.out.println("\n");
+        System.out.println("Categoria : " + categoria);
+        System.out.println("\n");
+        System.out.println("Estado : " + estado);
+        System.out.println("\n");
+        System.out.println("Precio  : " + estado);
+        System.out.println("\n--------------------------------");
+    }
+    public void mostrar(String[] args) {
+        /*Producto unproducto = new Producto();
+        unproducto.codigo = 24;
+        unproducto.descripcion = "Matambre con guarnicion";
+        unproducto.estado = "Disponible";
+        unproducto.precio = 2.45f;
+        System.out.println(unproducto);*/
     }
 }
