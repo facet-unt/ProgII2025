34ebeab0d435eb21ff51832772483d604afc714f
norryianfranco <norryianfranco@gmail.com>
2025-09-16 19:04:32 -0300

    Se agregaron las funciones ver y asignar y los constructores a cada clase. Se creo la etiqueta para github

--- src/usuarios/modelos/Cliente.java
+++ src/usuarios/modelos/Cliente.java
@@ -9,7 +9,7 @@ package usuarios.modelos;
  * @author estudiante
  */
 public class Cliente {
-    public String correo, clave, apellido, nombre;
+    private String correo, clave, apellido, nombre;
     
     public void mostrar(){
         System.out.println("Los datos de este cliente son:");
@@ -17,5 +17,52 @@ public class Cliente {
         System.out.println("Nombre: "+nombre);
         System.out.println("Correo: "+correo);
         System.out.println("Clave: "+clave);
+    }
+    
+    public String verCorreo(){
+        return correo;
+    }
+    
+    public void asignarCorreo(String c) {
+        if (c != null && !c.isBlank()){
+            correo = c;
+        }
+    }
+    
+    public String verClave(){
+        return clave;
+    }
+    
+    public void asignarClave(String c) {
+        if (c != null && !c.isBlank()){
+            clave = c;
+        }
+    }
+    
+    public String verNombre(){
+        return nombre;
+    }
+    
+    public void asignarNombre(String n) {
+        if (n != null && !n.isBlank()){
+            nombre = n;
+        }
+    }
+    
+    public String verApellido(){
+        return apellido;
+    }
+    
+    public void asignarApellido(String a) {
+        if (a != null && !a.isBlank()){
+            apellido = a;
+        }
+    }
+
+    public Cliente(String correo, String clave, String apellido, String nombre) {
+        this.correo = correo;
+        this.clave = clave;
+        this.apellido = apellido;
+        this.nombre = nombre;
     }
 }
--- src/usuarios/modelos/Encargado.java
+++ src/usuarios/modelos/Encargado.java
@@ -18,4 +18,54 @@ public class Encargado {
         System.out.println("Correo: "+correo);
         System.out.println("Clave: "+clave);
     }
+    
+    
+    public String verCorreo(){
+        return correo;
+    }
+    
+    public void asignarCorreo(String c) {
+        if (c != null && !c.isBlank()){
+            correo = c;
+        }
+    }
+    
+    public String verClave(){
+        return clave;
+    }
+    
+    public void asignarClave(String c) {
+        if (c != null && !c.isBlank()){
+            clave = c;
+        }
+    }
+    
+    public String verNombre(){
+        return nombre;
+    }
+    
+    public void asignarNombre(String n) {
+        if (n != null && !n.isBlank()){
+            nombre = n;
+        }
+    }
+    
+    public String verApellido(){
+        return apellido;
+    }
+    
+    public void asignarApellido(String a) {
+        if (a != null && !a.isBlank()){
+            apellido = a;
+        }
+    }
+    
+    
+
+    public Encargado(String correo, String clave, String apellido, String nombre) {
+        this.correo = correo;
+        this.clave = clave;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
 }
