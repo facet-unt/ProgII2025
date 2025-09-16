9dfc327cbdad89f1cf13c7fe8da7aca63550ffa6
msanchez <msanchez>
2025-09-16 10:05:00 -0300

    Controlador para tp2 parte2

--- src/principal/controladores/ControladorPrincipal.java
+++ src/principal/controladores/ControladorPrincipal.java
@@ -1,11 +1,36 @@
 package principal.controladores;
 
+import java.util.ArrayList;
 import productos.modelos.Producto;
 
 
 public class ControladorPrincipal {
     public static void main(String[] args) {
-        Producto p = new Producto();
-        p.mostrar();
+        ArrayList<Producto> listaProductos = new ArrayList<>();
+        
+        System.out.println("#################### ");
+        System.out.println("PRODUCTOS");
+        Producto p1 = new Producto(1, "Producto1", "Plato Principal", "Disponible", 1550.8f);
+        Producto p2= new Producto(2, "Producto2", "Postre", "Disponible", 850.8f);
+        Producto p3 = new Producto(3, "Producto3", "Plato Principal", "No Disponible", 1050.0f);
+        
+        listaProductos.add(p1);
+        listaProductos.add(p2);
+        listaProductos.add(p3);
+        
+        System.out.println("PRODUCTOS usando mostrar()");
+        for (Producto p: listaProductos)
+            p.mostrar();
+         
+        listaProductos.get(2).asignarDescripcion("Producto 3");
+        System.out.println("\nEl precio del producto es :" +  listaProductos.get(2).verPrecio());
+        listaProductos.get(2).asignarPrecio(1898.98f);
+        System.out.println("El nuevo precio del producto es :" +  listaProductos.get(2).verPrecio());
+        
+        System.out.println("\nPRODUCTOS usando toString()");
+        for (Producto p: listaProductos)
+            System.out.println(p);
+        System.out.println("#################### ");
+ 
     }
 }
