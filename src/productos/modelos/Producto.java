0a52e3525101367744c441ff4605d9a5d7cc15d1
Agustin Arborio <agusarborio@gmail.com>
2025-09-16 19:02:50 -0300

    modificacion de las clases para el uso de get, set y constructor (falta asignar valores y mostrarlo, a partir del punto7)

--- src/productos/modelos/Producto.java
+++ src/productos/modelos/Producto.java
@@ -1,8 +1,77 @@
 package productos.modelos;
 
-
+/**
+ *
+ * @author estudiante
+ */
 public class Producto {
-    public void mostrar() {
-        System.out.println("Soy un producto");
+    private int codigo;
+    private String descripcion;
+    private String categoria;
+    private String estado;
+    private float precio;
+
+    public Producto(int codigo, String descripcion, String categoria, String estado, float precio) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+        this.estado = estado;
+        this.precio = precio;
+    }
+    
+    
+    
+    public void mostrar(){
+        
+        //System.out.println("Hola, soy un producto");
+        System.out.println(toString());
+    }
+    //Agregar to string
+
+    @Override
+    public String toString() {
+        return "Producto{" + "codigo=" + codigo + ", descripcion=" + descripcion + ", categoria=" + categoria + ", estado=" + estado + ", precio=" + precio + '}';
+    }
+
+    public int verCodigo() {
+        return codigo;
+    }
+
+    public void asignarCodigo(int c) {
+        if (c > 0)
+            codigo = c;
+    }
+
+    public String verDescripcion() {
+        return descripcion;
+    }
+
+    public void asignarDescripcion(String d) {
+        if (d != null && !d.isBlank())
+            descripcion = d;
+    }
+
+    public String verCategoria() {
+        return categoria;
+    }
+
+    public void asignarCategoria(String c) {
+        categoria = c;
+    }
+
+    public String verEstado() {
+        return estado;
+    }
+
+    public void asignarEstado(String e) {
+        estado = e;
+    }
+
+    public float verPrecio() {
+        return precio;
+    }
+
+    public void asignarPrecio(float p) {
+        precio = p;
     }
 }
