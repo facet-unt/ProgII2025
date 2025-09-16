dccb2ceacad26ea39acc9c39e170c8335bf5e101
Llobeta Salustiano José <salustianollobeta2005@gmail.com>
2025-09-16 16:41:26 -0300

    Modificar Producto.java (añadir get/set y constructor)

--- src/productos/modelos/Producto.java
+++ src/productos/modelos/Producto.java
@@ -1,15 +1,63 @@
 package productos.modelos;
 
 
-public class Producto {
-    public int codigo;
-    public String descripcion;
-    public String categoria;
-    public String estado;
-    public float precio;
+public class Producto{
+    private int codigo;
+    private String descripcion;
+    private String categoria;
+    private String estado;
+    private float precio;
+
+    public Producto(int c, String d, String cat, String e, float p){
+        codigo=c;
+        descripcion=d;
+        categoria=cat;
+        estado=e;
+        precio=p;
+    }
+    
+    public int verCodigo() {
+        return codigo;
+    }
+
+    public void asignarCodigo(int c) {
+        codigo = c;
+    }
+
+    public String verDescripcion() {
+        return descripcion;
+    }
+
+    public void asignarDescripcion(String d) {
+        descripcion = d;
+    }
+
+    public String verCategoria() {
+        return categoria;
+    }
+
+    public void asignarCategoria(String c) {
+        categoria = c;
+    }
+
+    public String verEstado() {
+        return estado;
+    }
+
+    public void asignarEstado(String e) {
+        estado = e;
+    }
+
+    public float verPrecio() {
+        return precio;
+    }
+
+    public void asignarPrecio(float p) {
+        precio = p;
+    }
+    
     
     public void mostrar() {
-      
         System.out.println("Soy un producto");
         System.out.println("codigo: "+codigo);
         System.out.println("descripcion: "+descripcion);
@@ -17,6 +65,7 @@ public class Producto {
         System.out.println("estado: "+estado);
         System.out.println("precio: "+precio);
     }
+    
     @Override
     public String toString() {
         return "Producto{" + "codigo=" + codigo + ", descripcion=" + descripcion + ", categoria=" + categoria + ", estado=" + estado + ", precio=" + precio + '}';
