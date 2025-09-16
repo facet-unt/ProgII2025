5a6698fa43d0444d73f36dbf1d9b315e43b923ab
estudiante <ortizfacundo58@gmail.com>
2025-09-16 18:54:45 -0300

    Encapsulado de variables de clases cliente, empleado y encargado

--- src/usuarios/modelos/Cliente.java
+++ src/usuarios/modelos/Cliente.java
@@ -9,13 +9,63 @@ package usuarios.modelos;
  * @author estudiante
  */
 public class Cliente {
-    public String correo;
-    public String clave;
-    public String apellido;
-    public String nombre;
+    //pase a privado los atributos 
+    private String correo;
+    private String clave;
+    private String apellido;
+    private String nombre;
 // lo  nuestro
     public void mostrar(){
+        System.out.println("Soy un Cliente");
+        System.out.println("Correo: " + correo);
+        System.out.println("Clave: " + clave);
+        System.out.println("apellido: " + apellido);
+        System.out.println("Nombre: " + nombre);
     }
+
+    public Cliente(String correo, String clave, String apellido, String nombre) {
+        this.correo = correo;
+        this.clave = clave;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
+
+    public String verCorreo() {
+        return correo;
+    }
+
+    public void asignarCorreo(String c) {
+        if (c != null && !c.isBlank())
+            correo = c;
+    }
+
+    public String verClave() {
+        return clave;
+    }
+
+    public void asignarClave(String c) {
+        if (c != null && !c.isBlank())
+            correo = c;
+    }
+
+    public String verApellido() {
+        return apellido;
+    }
+
+    public void asignarApellido(String a) {
+        apellido = a;
+    }
+
+    public String verNombre() {
+        return nombre;
+    }
+
+    public void asignarNombre(String n) {
+        nombre = n;
+    }
+    
+    
+    
 // lo del ingeniero
     
     //void mostrar();
--- src/usuarios/modelos/Encargado.java
+++ src/usuarios/modelos/Encargado.java
@@ -17,11 +17,58 @@ public class Encargado {
     public void mostrar(){
     }*/
 // lo del ingeniero
-    String correo;
-    String clave;
-    String apellido;
-    String nombre;
+    private String correo;
+    private String clave;
+    private String apellido;
+    private String nombre;
     
-   // void mostrar();
+    public void mostrar(){
+        System.out.println("Soy un Cliente");
+        System.out.println("Correo: " + correo);
+        System.out.println("Clave: " + clave);
+        System.out.println("apellido: " + apellido);
+        System.out.println("Nombre: " + nombre);
+    }
 
+    public String verCorreo() {
+        return correo;
+    }
+
+    public void asignarCorreo(String c) {
+        if (c != null && !c.isBlank())
+            correo = c;
+    }
+
+    public String verClave() {
+        return clave;
+    }
+
+    public void asignarClave(String c) {
+        if (c != null && !c.isBlank())
+            correo = c;
+    }
+
+    public String verApellido() {
+        return apellido;
+    }
+
+    public void asignarApellido(String a) {
+        apellido = a;
+    }
+
+    public String verNombre() {
+        return nombre;
+    }
+
+    public void asignarNombre(String n) {
+        nombre = n;
+    }
+
+    public Encargado(String correo, String clave, String apellido, String nombre) {
+        this.correo = correo;
+        this.clave = clave;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
+    
 }
