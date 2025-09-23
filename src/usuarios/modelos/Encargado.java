407d186bc7cae3f5354fe16ef133613c6fd5c375
PedroCagna <pedricagna9@gmail.com>
2025-09-23 17:17:31 -0300

    Se deja como comentarios las funciones void mostrar

--- src/usuarios/modelos/Encargado.java
+++ src/usuarios/modelos/Encargado.java
@@ -59,14 +59,14 @@ public class Encargado {
         this.clave = clave;
     }
     
-    public void mostrar(){
-        System.out.println("-- ENCARGADO --");
-        System.out.println("Apellido: " + apellido);
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Correo: " + correo);
-        System.out.println("Clave: " + clave);
-     } 
-    //Preguntar si deberia eliminar el void mostrar
+//    public void mostrar(){
+//        System.out.println("-- ENCARGADO --");
+//        System.out.println("Apellido: " + apellido);
+//        System.out.println("Nombre: " + nombre);
+//        System.out.println("Correo: " + correo);
+//        System.out.println("Clave: " + clave);
+//     } 
+    
     @Override
     public String toString() {
         return "Encargado{" + "apellido=" + apellido + ", nombre=" + nombre + ", correo=" + correo + ", clave=" + clave + '}';
