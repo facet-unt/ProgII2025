cb078867786e7834ccfb360c570717db5d5c0324
Gaston Romano <romanogaston13@gmail.com>
2025-09-23 18:10:08 -0300

    Implemento ControladorPrincipal(con esto, actualizo Producto)

--- src/principal/controladores/ControladorPrincipal.java
+++ src/principal/controladores/ControladorPrincipal.java
@@ -1,9 +1,7 @@
 package principal.controladores;
 
-import java.util.ArrayList;
 import productos.modelos.Producto;
 import java.util.ArrayList;
-import usuarios.modelos.*;
 
 public class ControladorPrincipal {
     public static void main(String[] args) {
