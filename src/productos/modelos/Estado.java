e8fb8ccc60f708d36585f7686743eaf0f342e92f
Lazaro Mauricio Rojas <lazarojas.facet@gmail.com>
2025-09-24 22:58:18 -0300

    Creacion de las enumeraciones Categoria y Estado

---
+++ src/productos/modelos/Estado.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package productos.modelos;
+
+/**
+ *
+ * @author lazar
+ */
+public enum Estado {
+    DISPONIBLE("Disponible"),
+    NO_DISPONIBLE("No Disponible");
+    
+    private String valor;
+
+    private Estado(String valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return this.valor;
+    }
+}
