211bbb78c5896bb55c6ca468dfa30e0ab75783bf
gabinete <gabineten@gmail.com>
2025-09-30 15:37:39 -0300

    Se agregaron metodos get/set

--- src/pedidos/modelos/ProductoDelPedido.java
+++ src/pedidos/modelos/ProductoDelPedido.java
@@ -17,6 +17,22 @@ public class ProductoDelPedido {
         this.cantidad = cantidad;
         this.producto = producto;
     }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
     
     
 }
