6c7e640162e6bc00edb586792c7ed51fa1624ad6
gabinete <gabineten@gmail.com>
2025-09-30 15:36:32 -0300

    Se crearon los constructores de las variables de instancia

--- src/pedidos/modelos/ProductoDelPedido.java
+++ src/pedidos/modelos/ProductoDelPedido.java
@@ -12,4 +12,11 @@ import productos.modelos.Producto;
 public class ProductoDelPedido {
     private int cantidad;
     private Producto producto;
+
+    public ProductoDelPedido(int cantidad, Producto producto) {
+        this.cantidad = cantidad;
+        this.producto = producto;
+    }
+    
+    
 }
