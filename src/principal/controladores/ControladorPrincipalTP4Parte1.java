80b44d8ab81a5f043a4e16eb8162884aa0d73010
gabinete <gabineten@gmail.com>
2025-10-07 16:52:03 -0300

    Se agrego la lista de los productos y la sentencia para mostrar la lista

--- src/principal/controladores/ControladorPrincipalTP4Parte1.java
+++ src/principal/controladores/ControladorPrincipalTP4Parte1.java
@@ -33,109 +33,107 @@ public class ControladorPrincipalTP4Parte1 {
         if (!listaProductos.contains(p1)) {
             listaProductos.add(p1);
         }
+        if (!listaProductos.contains(p2)) {
+            listaProductos.add(p2);
+        }
+        if (!listaProductos.contains(p3)) {
+            listaProductos.add(p3);
+        }
+        if (!listaProductos.contains(p4)) {
+            listaProductos.add(p4);
+        }
+
+        for (Producto p : listaProductos) {
+            p.mostrar();
+        }
+
+        System.out.println();
+        System.out.println("\n#################### ");
+        System.out.println("CLIENTES");
+        Cliente cliente1 = new Cliente("cliente1@bar.com", "claveCliente1", "ApellidoCliente1", "NombreCliente1");
+        Cliente cliente2 = new Cliente("cliente2@bar.com", "claveCliente2", "ApellidoCliente2", "NombreCliente2");
+        Cliente cliente3 = new Cliente("cliente3@bar.com", "claveCliente3", "ApellidoCliente3", "NombreCliente3");
+
+        listaClientes.add(cliente1);
+        listaClientes.add(cliente2);
+        listaClientes.add(cliente3);
         /*
-        **************************************************
-         * AGREGAR aqui las sentencias para completar el llenado de 
-         * la lista de productos
-        **************************************************
-         */
-      
-
-        /*
-        **************************************************
-         * AGREGAR aqui las sentencias para mostrar la lista
-         * de productos, deberían listarse 4
-        **************************************************
-         */
-      
-
-            System.out.println();
-            System.out.println("\n#################### ");
-            System.out.println("CLIENTES");
-            Cliente cliente1 = new Cliente("cliente1@bar.com", "claveCliente1", "ApellidoCliente1", "NombreCliente1");
-            Cliente cliente2 = new Cliente("cliente2@bar.com", "claveCliente2", "ApellidoCliente2", "NombreCliente2");
-            Cliente cliente3 = new Cliente("cliente3@bar.com", "claveCliente3", "ApellidoCliente3", "NombreCliente3");
-
-            listaClientes.add(cliente1);
-            listaClientes.add(cliente2);
-            listaClientes.add(cliente3);
-            /*
     Clientes, se crean 3 para crear los pedidos
-             */
-            for (Cliente e : listaClientes) {
-                e.mostrar();
-            }
+         */
+        for (Cliente e : listaClientes) {
+            e.mostrar();
+        }
 
-            System.out.println();
-            System.out.println("#################### ");
-            System.out.println("PEDIDOS");
-            ArrayList<ProductoDelPedido> listapdp1 = new ArrayList<>();
-            ProductoDelPedido pdp1 = new ProductoDelPedido(listaProductos.get(0), 1);
-            ProductoDelPedido pdp2 = new ProductoDelPedido(listaProductos.get(1), 2);
-            ProductoDelPedido pdp3 = new ProductoDelPedido(listaProductos.get(1), 8);
-            if (!listapdp1.contains(pdp1)) {
-                listapdp1.add(pdp1);
-            }
-            if (!listapdp1.contains(pdp2)) {
-                listapdp1.add(pdp2);
-            }
-            if (!listapdp1.contains(pdp3)) {
-                listapdp1.add(pdp3);
-            }
-            Pedido unPedido1 = new Pedido(1, LocalDateTime.now(), listapdp1, cliente1);
+        System.out.println();
+        System.out.println("#################### ");
+        System.out.println("PEDIDOS");
+        ArrayList<ProductoDelPedido> listapdp1 = new ArrayList<>();
+        ProductoDelPedido pdp1 = new ProductoDelPedido(listaProductos.get(0), 1);
+        ProductoDelPedido pdp2 = new ProductoDelPedido(listaProductos.get(1), 2);
+        ProductoDelPedido pdp3 = new ProductoDelPedido(listaProductos.get(1), 8);
+        if (!listapdp1.contains(pdp1)) {
+            listapdp1.add(pdp1);
+        }
+        if (!listapdp1.contains(pdp2)) {
+            listapdp1.add(pdp2);
+        }
+        if (!listapdp1.contains(pdp3)) {
+            listapdp1.add(pdp3);
+        }
+        Pedido unPedido1 = new Pedido(1, LocalDateTime.now(), listapdp1, cliente1);
 
-            ArrayList<ProductoDelPedido> productosDelPedido2 = new ArrayList<>();
-            ProductoDelPedido pdp4 = new ProductoDelPedido(listaProductos.get(2), 10);
-            ProductoDelPedido pdp5 = new ProductoDelPedido(listaProductos.get(0), 20);
-            ProductoDelPedido pdp6 = new ProductoDelPedido(listaProductos.get(2), 30);
-            //producto repetido        
-            if (!productosDelPedido2.contains(pdp4)) {
-                productosDelPedido2.add(pdp4);
-            }
-            if (!productosDelPedido2.contains(pdp5)) {
-                productosDelPedido2.add(pdp5);
-            }
-            if (!productosDelPedido2.contains(pdp6)) {
-                productosDelPedido2.add(pdp6);
-            }
-            Pedido unPedido2 = new Pedido(2, LocalDateTime.now(), productosDelPedido2, cliente2);
+        ArrayList<ProductoDelPedido> productosDelPedido2 = new ArrayList<>();
+        ProductoDelPedido pdp4 = new ProductoDelPedido(listaProductos.get(2), 10);
+        ProductoDelPedido pdp5 = new ProductoDelPedido(listaProductos.get(0), 20);
+        ProductoDelPedido pdp6 = new ProductoDelPedido(listaProductos.get(2), 30);
+        //producto repetido        
+        if (!productosDelPedido2.contains(pdp4)) {
+            productosDelPedido2.add(pdp4);
+        }
+        if (!productosDelPedido2.contains(pdp5)) {
+            productosDelPedido2.add(pdp5);
+        }
+        if (!productosDelPedido2.contains(pdp6)) {
+            productosDelPedido2.add(pdp6);
+        }
+        Pedido unPedido2 = new Pedido(2, LocalDateTime.now(), productosDelPedido2, cliente2);
 
-            ArrayList<ProductoDelPedido> productosDelPedido3 = new ArrayList<>();
-            ProductoDelPedido pdp7 = new ProductoDelPedido(listaProductos.get(1), 100);
-            ProductoDelPedido pdp8 = new ProductoDelPedido(listaProductos.get(2), 200);
-            if (!productosDelPedido3.contains(pdp7)) {
-                productosDelPedido3.add(pdp7);
-            }
-            if (!productosDelPedido3.contains(pdp8)) {
-                productosDelPedido3.add(pdp8);
-            }
-            Pedido unPedido3 = new Pedido(2, LocalDateTime.now(), productosDelPedido3, cliente3);
-            //pedido repetido no debe agregarse a la lista
+        ArrayList<ProductoDelPedido> productosDelPedido3 = new ArrayList<>();
+        ProductoDelPedido pdp7 = new ProductoDelPedido(listaProductos.get(1), 100);
+        ProductoDelPedido pdp8 = new ProductoDelPedido(listaProductos.get(2), 200);
+        if (!productosDelPedido3.contains(pdp7)) {
+            productosDelPedido3.add(pdp7);
+        }
+        if (!productosDelPedido3.contains(pdp8)) {
+            productosDelPedido3.add(pdp8);
+        }
+        Pedido unPedido3 = new Pedido(2, LocalDateTime.now(), productosDelPedido3, cliente3);
+        //pedido repetido no debe agregarse a la lista
 
-            if (!listaPedidos.contains(unPedido1)) {
-                listaPedidos.add(unPedido1);
-            }
-            if (!listaPedidos.contains(unPedido2)) {
-                listaPedidos.add(unPedido2);
-            }
-            if (!listaPedidos.contains(unPedido3)) {
-                listaPedidos.add(unPedido3);
-            }
+        if (!listaPedidos.contains(unPedido1)) {
+            listaPedidos.add(unPedido1);
+        }
+        if (!listaPedidos.contains(unPedido2)) {
+            listaPedidos.add(unPedido2);
+        }
+        if (!listaPedidos.contains(unPedido3)) {
+            listaPedidos.add(unPedido3);
+        }
 
-            System.out.println("Los pedidos son: ");
-            System.out.println("=======");
-            /*
+        System.out.println("Los pedidos son: ");
+        System.out.println("=======");
+        /*
         *************************************************
         AGREGAR aqui la sentencia para mostrar la lista de pedidos
         *************************************************
-             */
+         */
 
-            System.out.println();
+        System.out.println();
 
-            /*
+        /*
         *************************************************
          FIN Primera parte - comparación de objetos
         ***************************************************
-             */
-        }
+         */
     }
+}
