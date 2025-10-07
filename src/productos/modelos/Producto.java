6508cfe3453214a33a74a8384a69206c05c2deec
Juan Barrionuevo <juampi4112017@gmail.com>
2025-10-07 16:05:57 -0300

    Resolución de los conflictos con el paquete de modelos.productos

--- src/productos/modelos/Producto.java
+++ src/productos/modelos/Producto.java
@@ -12,28 +12,19 @@ public class Producto {
     //Atributos
     private int codigo;
     private String descripcion;
-<<<<<<< HEAD
     private Categoria unaCategoria;
     private Estado unEstado;
-=======
->>>>>>> desarrollo
     private float precio;
-    private Categoria categoria;
-    private Estado estado;
+
 
     // Métodos
     public void mostrar() {
         System.out.println("Codigo: " + codigo);
         System.out.println("Descripcion: " + descripcion);
-<<<<<<< HEAD
         System.out.println("Categoria: " + unaCategoria.verValor());
         System.out.println("Estado: " + unEstado.verValor());
         System.out.println("Precio: " + precio);
-=======
-        System.out.println("Precio: " + precio);
-        System.out.println("Categoria: " + categoria);
-        System.out.println("Estado: " + estado);
->>>>>>> desarrollo
+
     }
 
     @Override
@@ -46,7 +37,7 @@ public class Producto {
                 ", precio=" + precio +
                 '}';
     }
-<<<<<<< HEAD
+
     
     public Producto(int codigo, String descripcion, Categoria categoria, Estado estado, float precio) {
         this.codigo = codigo;
@@ -61,27 +52,6 @@ public class Producto {
         this.unEstado =  Estado.valueOf(estado.toUpperCase());        
         this.descripcion = descripcion;
         this.unaCategoria =  Categoria.valueOf(categoria.toUpperCase());
-=======
-
-    public Producto(int codigo, String descripcion, Categoria categoria, Estado estado, float precio) {
-        this.codigo = codigo;
-        this.descripcion = descripcion;
->>>>>>> desarrollo
-        this.precio = precio;
-        this.categoria = categoria;
-        this.estado = estado;
-    }
-<<<<<<< HEAD
-    
-    public Producto(int c, String d, Categoria cat, float p) {
-        codigo = c;
-        descripcion = d;
-        unaCategoria = cat;
-        precio = p;
-    }
-    
-    public Producto(int c, String d, float p, Categoria cat) {
-        this(c, d,cat, p);        
     }
 
     
@@ -108,7 +78,7 @@ public class Producto {
     }
 
     public Categoria verCategoria() {
-<<<<<<< HEAD
+
         return unaCategoria;
     }
 
@@ -122,21 +92,7 @@ public class Producto {
 
     public void asignarEstado(Estado e) {
         unEstado = e;
-=======
-        return categoria;
-    }
 
-    public void asignarCategoria(Categoria c) {
-        categoria = c;
-    }
-
-    public Estado verEstado() {
-        return estado;
-    }
-
-    public void asignarEstado(Estado e) {
-            estado = e;
->>>>>>> desarrollo
     }
 
     public float verPrecio() {
