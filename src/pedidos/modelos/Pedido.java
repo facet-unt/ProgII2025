d1cfb58ee76bdf2e33b3a95ac5ba07d4e6eb1b50
Irina Caramuti <iricaramuti@gmail.com>
2025-10-07 15:58:13 -0300

    Método Equals y HashCode clase Pedido

--- src/pedidos/modelos/Pedido.java
+++ src/pedidos/modelos/Pedido.java
@@ -134,4 +134,28 @@ public class Pedido {
         productoPedido.add(unProductoDelPedido);
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + this.numero;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pedido other = (Pedido) obj;
+        return this.numero == other.numero;
+    }
+    
+    
+
 }
