6ba1678f5a13fa001a48e8dcdbebf9769b69cadb
Agustin Arborio <agsuarborio@gmail.com>
2025-10-08 12:42:28 -0300

    Fin parte 2 tp4

--- src/usuarios/modelos/Encargado.java
+++ src/usuarios/modelos/Encargado.java
@@ -6,69 +6,20 @@ package usuarios.modelos;
 
 
 
-public class Encargado{
+public class Encargado extends Usuario{
     
-    public String correo;
-    public String clave;
-    public String apellido;
-    public String nombre;
-
-    public Encargado (String correo, String clave, String apellido, String nombre) {
-        this.correo = correo;
-        this.clave = clave;
-        this.apellido = apellido;
-        this.nombre = nombre; 
+   public Encargado (String correo, String clave, String apellido, String nombre) {
+        super(correo, clave, nombre, apellido); 
     }
-    
-    // Método para agregar un pedido al cliente
-   
+
+    @Override
     public void mostrar(){
         
     }
-    
+
     @Override
     public String toString() {
-        return "Cliente{" + "correo=" + correo + ", clave=" + clave + ", apellido=" + apellido + ", nombre=" + nombre + '}';
-    }
-
-    // Get y set
-    public String verCorreo() {
-        return correo;
-    }
-   
-    
-    //esto se llama get, es para otener al objeto
-    
-    public void asignarCorreo (String Correo) {
-        if (Correo != null && !Correo.isBlank())
-            correo = Correo;
-    }
-    
-    public String verClave() {
-        return clave;
-    }
-    
-    public void asignarClave(String Clave) {
-        if (Clave != null && !Clave.isBlank())
-            clave = Clave;
-    }
-    
-    public String verNombre() {
-        return nombre;
-    }
-    
-    public void asignarNombre(String Nombre) {
-        if (Nombre != null && !Nombre.isBlank())
-            nombre = Nombre;
-    }
-    
-    public String verApellido() {
-        return apellido;
-    }
-    
-    public void asignarApellido(String Apellido) {
-        if (Apellido != null && !Apellido.isBlank())
-            apellido = Apellido;
+        return super.toString(); 
     }
 }
 
