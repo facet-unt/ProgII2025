ea53ce84af0bd5811230b3a7e0d608151e03f534
Ssyhakar Natanael Josue <115747734+NatSsy@users.noreply.github.com>
2025-10-09 11:36:09 -0300

    Hicimos a la clase Empleado subclase de Usuario

--- src/usuarios/modelos/Empleado.java
+++ src/usuarios/modelos/Empleado.java
@@ -9,56 +9,10 @@ package usuarios.modelos;
  * @author estudiante
  */
 
-public class Empleado {
-   private String correo;
-   private String clave;
-   private String apellido;
-   private String nombre;
-   
-    public void mostrar() {
-      System.out.println(correo);
-      System.out.println(clave);
-      System.out.println(apellido);
-      System.out.println(nombre);
-    }
-    
-    public Empleado(String c, String cl, String a, String n) {
-        correo = c;
-        clave = cl;
-        apellido = a;
-        nombre = n;
+public class Empleado extends Usuario {
+
+    public Empleado(String correo, String clave, String apellido, String nombre) {
+        super(correo, clave, apellido, nombre);
     }
 
-    public String verCorreo() {
-        return correo;
-    }
-
-    public void asignarCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public String verClave() {
-        return clave;
-    }
-
-    public void asignarClave(String clave) {
-        this.clave = clave;
-    }
-
-    public String verApellido() {
-        return apellido;
-    }
-
-    public void asignarApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String verNombre() {
-        return nombre;
-    }
-
-    public void asignarNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    
 }
