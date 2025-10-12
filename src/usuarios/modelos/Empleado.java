6bee1401e9a8a84e2a61a1618a163c2a6c5e8b60
Lucas <90874300+vvintra@users.noreply.github.com>
2025-10-12 17:59:16 -0300

    se agrego y redifinio en las clases Empleado y Encargado el método verPedidos() para que devuelva un ArrayList vacío

--- src/usuarios/modelos/Empleado.java
+++ src/usuarios/modelos/Empleado.java
@@ -4,6 +4,9 @@
  */
 package usuarios.modelos;
 
+import java.util.ArrayList;
+import pedidos.modelos.Pedido;
+
 /**
  *
  * @author estudiante
@@ -13,6 +16,11 @@ public class Empleado extends Usuario {
 
     public Empleado(String correo, String clave, String apellido, String nombre) {
         super(correo, clave, apellido, nombre);
+    }
+
+    @Override
+    public ArrayList<Pedido> verPedidos() {
+        return new ArrayList<>();
     }
 
 }
