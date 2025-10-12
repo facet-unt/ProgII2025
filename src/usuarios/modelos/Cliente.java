0ee518699246cc8f02607dd2dcb688533c30557a
Lucas <90874300+vvintra@users.noreply.github.com>
2025-10-12 17:17:01 -0300

    Se agrego el metodo abstracto de la superclase y la ArrayList<Pedido> pedido en Cliente

--- src/usuarios/modelos/Cliente.java
+++ src/usuarios/modelos/Cliente.java
@@ -12,11 +12,18 @@ import pedidos.modelos.Pedido;
  * @author estudiante
  */
 public class Cliente extends Usuario {
+    
+    private ArrayList<Pedido> pedido;
 
     public Cliente(String correo, String clave, String apellido, String nombre) {
         super(correo, clave, apellido, nombre);
     }
     public void mostrar() {
         super.mostrar();
+    }
+
+    @Override
+    public ArrayList<Pedido> verPedidos() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
