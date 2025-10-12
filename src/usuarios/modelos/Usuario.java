33847cb111bae7f443665e87acf1dfdeb5f1ff43
adolfo lautaro mamaní <adolfo.lau.mamani@gmail.com>
2025-10-12 12:49:31 -0300

    creacion de clase Usuario e implementacion de dicha clase

---
+++ src/usuarios/modelos/Usuario.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package usuarios.modelos;
+
+/**
+ *
+ * @author karen
+ */
+public abstract class Usuario {
+    private String correo;
+    private String clave;
+    private String apellido;
+    private String nombre;
+    
+    //Metodos
+    public void mostrar(){
+        System.out.println("=================================");
+        System.out.println("apellido: "+verApellido()); 
+        System.out.println("nombre: "+verNombre());
+        System.out.println("correo: "+verCorreo());
+        System.out.println("clave: "+verClave());
+        System.out.println("=================================");
+    }
+    //constructor
+
+    public Usuario(String correo, String clave, String apellido, String nombre) {
+        this.correo = correo;
+        this.clave = clave;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
+    
+    //metodos get/set
+    public String verCorreo() {
+        return correo;
+    }
+
+    public void asignarCorreo(String correo) {
+        if (correo != null && !correo.isBlank()){
+            this.correo = correo;
+        }
+    }
+
+    public String verClave() {
+        return clave;
+    }
+
+    public void asignarClave(String clave) {
+        if (clave != null && !clave.isBlank()){
+            this.clave = clave;
+        }
+    }
+
+    public String verApellido() {
+        return apellido;
+    }
+
+    public void asignarApellido(String apellido) {
+        if (apellido != null && !apellido.isBlank()){
+            this.apellido = apellido;
+        }
+    }
+
+    public String verNombre() {
+        return nombre;
+    }
+
+    public void asignarNombre(String nombre) {
+        if (nombre != null && !nombre.isBlank()){
+            this.nombre = nombre;
+        }
+    }
+    
+    
+}
