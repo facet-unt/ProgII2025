357218e3ef3f13f8593550bcbf3320b2670247c2
juan pablo <troolorf@gmail.com>
2025-10-14 12:15:47 -0300

    modificacion del main pt3 del tp4

--- src/principal/controladores/ControladorPrincipalTP4Parte3.java
+++ src/principal/controladores/ControladorPrincipalTP4Parte3.java
@@ -33,11 +33,12 @@ public class ControladorPrincipalTP4Parte3 {
         
         if (!usuarios.contains(unCliente1))
             usuarios.add(unCliente1);
-        /*
-            *************************************************
-            AGREGAR aqui las sentencias para sumar a la lista el resto de los clientes 
-            *************************************************
-        */ 
+        if (!usuarios.contains(unCliente2))
+            usuarios.add(unCliente2);
+        if (!usuarios.contains(unCliente3))
+            usuarios.add(unCliente3);
+        if (!usuarios.contains(unCliente4))
+            usuarios.add(unCliente4);
                         
         Usuario unEmpleado1 = new Empleado("empleado1@bar.com", "claveEmpleado1", "ApellidoEmpleado1", "NombreEmpleado1");        
         Usuario unEmpleado2 = new Empleado("empleado2@bar.com", "claveEmpleado2", "ApellidoEmpleado2", "NombreEmpleado2");        
@@ -45,12 +46,15 @@ public class ControladorPrincipalTP4Parte3 {
         Usuario unEmpleado4 = new Empleado("empleado3@bar.com", "claveEmpleado4", "ApellidoEmpleado4", "NombreEmpleado4");
         //empleado repetido
         
-        /*
-            *************************************************
-            AGREGAR aqui las sentencias para agregar los empleados a la lista de
-            usuarios verificando que NO se repitan
-            *************************************************
-        */
+         
+        if (!usuarios.contains(unEmpleado1))
+            usuarios.add(unEmpleado1);
+        if (!usuarios.contains(unEmpleado2))
+            usuarios.add(unEmpleado2);
+        if (!usuarios.contains(unEmpleado3))
+            usuarios.add(unEmpleado3);
+        if (!usuarios.contains(unEmpleado4))
+            usuarios.add(unEmpleado4);
         
         Usuario unEncargado1 = new Encargado("encargado1@bar.com", "claveEncargado1", "ApellidoEncargado1", "NombreEncargado1");
         Usuario unEncargado2 = new Encargado("encargado2@bar.com", "claveEncargado2", "ApellidoEncargado2", "NombreEncargado2");
@@ -58,12 +62,14 @@ public class ControladorPrincipalTP4Parte3 {
         Usuario unEncargado4 = new Encargado("encargado3@bar.com", "claveEncargado4", "ApellidoEncargado4", "NombreEncargado4");
         //encargado repetido
 
-        /*
-            *************************************************
-            AGREGAR aqui las sentencias para agregar los encargados a la lista de
-            usuarios verificando que NO se repitan
-            *************************************************
-        */
+          if (!usuarios.contains(unEncargado1))
+            usuarios.add(unEncargado1);
+        if (!usuarios.contains(unEncargado2))
+            usuarios.add(unEncargado2);
+        if (!usuarios.contains(unEncargado3))
+            usuarios.add(unEncargado3);
+        if (!usuarios.contains(unEncargado4))
+            usuarios.add(unEncargado4);
 
         Producto unProducto1 = new Producto(1, "Producto1", Categoria.ENTRADA, Estado.DISPONIBLE, 1.0f);        
         Producto unProducto2 = new Producto(2, "Producto2", Categoria.PLATOPRINCIPAL, Estado.DISPONIBLE, 2.0f);
@@ -71,14 +77,14 @@ public class ControladorPrincipalTP4Parte3 {
         Producto unProducto4 = new Producto(3, "Producto4", Categoria.POSTRE, Estado.DISPONIBLE, 4.0f);
         //producto repetido
         
-        if (!listaProductos.contains(p1))
-        listaProductos.add(p1); 
-        /*
-            **************************************************
-             * AGREGAR aqui las sentencias para completar el llenado de 
-             * la lista de productos
-            **************************************************
-        */
+        if (!listaProductos.contains(unProducto1))
+            listaProductos.add(unProducto1);
+        if (!listaProductos.contains(unProducto2))
+            listaProductos.add(unProducto2);
+        if (!listaProductos.contains(unProducto3))
+            listaProductos.add(unProducto3);
+        if (!listaProductos.contains(unProducto4))
+            listaProductos.add(unProducto4);
         
         ArrayList<ProductoDelPedido> listapdp1 = new ArrayList<>();
         ProductoDelPedido pdp1 = new ProductoDelPedido(listaProductos.get(0), 1);
