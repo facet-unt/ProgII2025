6c8f7de898a5b5476ba5b3c2feb71a28553304cf
Irina Caramuti <iricaramuti@gmail.com>
2025-10-27 18:43:57 -0300

    GestorProductos

--- src/productos/modelos/GestorProductos.java
+++ src/productos/modelos/GestorProductos.java
@@ -19,30 +19,88 @@ public class GestorProductos {
     }
     
     public String crearProducto(int codigo, String descripcion, float precio, Categoria categoria, Estado estado) {
-        return null;
+        Producto p = new Producto (codigo, descripcion, categoria, estado, precio);
+        if(codigo>0&&descripcion!=null&&precio>0&&categoria!=null&&estado!=null)
+        {
+            productos.add(p);
+            return ("Operación exitosa: El producto " + descripcion + " con código "+codigo +" categoría "+ categoria + " estado "+ estado + " y precio " +precio + " se guardó correctamente"); 
+            
+        }
+        else
+        {
+            return ("No se pudo realizar la Operación, ingrese valores válidos");
+        }
+        
     }
     
     public String modificarProducto(Producto p, int codigo, String descripcion, float precio, Categoria categoria, Estado estado) {
-        return null;
+        if (productos.contains(p))
+            {
+                productos.remove(p);
+            }
+        p.asignarCodigo(codigo);
+        p.asignarDescripcion(descripcion);
+        p.asignarPrecio(precio);
+        p.asignarCategoria(categoria);
+        p.asignarEstado(estado);
+        productos.add(p);
+        if(codigo>0&&descripcion!=null&&precio>0&&categoria!=null&&estado!=null)
+        {
+            productos.add(p);
+            return ("Operación exitosa: Los nuevos datos de producto son: descripción "+descripcion+", código "+codigo+", precio " +precio+", categoría "+categoria+", estado "+estado+"."); 
+            
+        }
+        else
+        {
+            return ("No se pudo realizar la Operación, ingrese valores válidos");
+        }
     }
     
     public ArrayList<Producto> menu() {
         return this.productos;
+        
     }
     
     public ArrayList<Producto> buscarProductos(String descripcion) {
-        return null;
+         
+        ArrayList<Producto> encontrados = new ArrayList<>();
+        for (Producto p : productos) {
+             
+            if (p.verDescripcion().toLowerCase().contains(descripcion.toLowerCase())) {
+                encontrados.add(p);
+            }
+                
+            }
+        return encontrados;
     }
     
     public boolean existeEsteProducto(Producto producto) {
-        return true;
+        if (productos.contains(producto))
+            return false;
+        else
+            return true;
     }
     
     public ArrayList<Producto> verProductosPorCategoria(Categoria categoria) {
-        return null;
+         ArrayList<Producto> prodcat = new ArrayList<>();
+        for (Producto p : productos) {
+             
+            if (p.verCategoria()==(categoria)) {
+                prodcat.add(p);
+            }
+                
+            }
+        return prodcat;
     }
     
     public Producto obtenerProducto(Integer codigo) {
+        for (Producto p : productos) {
+             
+            if (p.verCodigo()==(codigo)) {
+                return p;
+            }
+                
+            }
         return null;
     }
-}
+    }
