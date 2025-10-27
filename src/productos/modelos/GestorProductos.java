9ac9e7b6b1e93b0ad7d56a69909ab0a970d70d21
Llobeta Salustiano José <salustianollobeta2005@gmail.com>
2025-10-27 18:03:44 -0300

    Implementar método crearProducto() en GestorProductos.java

--- src/productos/modelos/GestorProductos.java
+++ src/productos/modelos/GestorProductos.java
@@ -4,6 +4,15 @@ import java.util.ArrayList;
 
 
 public class GestorProductos {
+    public static final String EXITO = "Producto creado/modificado con éxito";
+    public static final String ERROR_CODIGO = "El código del producto es incorrecto";
+    public static final String ERROR_DESCRIPCION = "La descripción del producto es incorrecta";
+    public static final String ERROR_PRECIO = "El precio del producto es incorrecto";
+    public static final String ERROR_CATEGORIA = "La categoría del producto es incorrecta";
+    public static final String ERROR_ESTADO = "El precio del producto es incorrecto";
+    public static final String PRODUCTOS_DUPLICADOS = "Ya existe un producto con ese código";
+    public static final String VALIDACION_EXITO = "Los datos del producto son correctos";
+    public static final String PRODUCTO_INEXISTENTE = "No existe el producto especificado";
     private ArrayList<Producto> productos = new ArrayList<>();
     
     private static GestorProductos instancia;
@@ -19,7 +28,22 @@ public class GestorProductos {
     }
     
     public String crearProducto(int codigo, String descripcion, float precio, Categoria categoria, Estado estado) {
-        return null;
+        if(codigo<=0){
+            return ERROR_CODIGO ;
+        }
+        if(descripcion==null||descripcion.isBlank()){
+            return ERROR_DESCRIPCION ;
+        }
+        if(precio<=0){
+            return ERROR_PRECIO ;
+        }
+        if(categoria==null){
+            return ERROR_CATEGORIA ;
+        }
+        if(estado==null){
+            return ERROR_ESTADO ;
+        }
+        return EXITO;
     }
     
     public String modificarProducto(Producto p, int codigo, String descripcion, float precio, Categoria categoria, Estado estado) {
