f7b3542b5327cfe59e912b0de0b0e3be934dfa9f
Llobeta Salustiano José <salustianollobeta2005@gmail.com>
2025-10-27 18:35:59 -0300

    Agregar Perfil.java en usuarios.modelos

---
+++ src/usuarios/modelos/Perfil.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package usuarios.modelos;
+
+/**
+ *
+ * @author salut
+ */
+public enum Perfil {
+    CLIENTE("Cliente"),
+    EMPLEADO("Empleado"),
+    ENCARGADO("Encargado");
+    
+    private String valor;
+
+    private Perfil(String valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return this.valor;
+    }
+}
