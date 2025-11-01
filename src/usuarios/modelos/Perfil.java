e40170329ab22dbb1432595029790174a4c31cea
Gaston Romano <romanogaston13@gmail.com>
2025-10-31 22:53:20 -0300

    Termino 2. del TP5 - Segunda Parte

---
+++ src/usuarios/modelos/Perfil.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package usuarios.modelos;
+
+/**
+ *
+ * @author Gaston
+ */
+public enum Perfil {
+    CLIENTE("Cliente"),
+    EMPLEADO("Empleado"),
+    ENCARGADO("Encargado");
+    
+    public String verValor() {
+        return valor;
+    }
+
+    public void asignarValor(String valor) {
+        this.valor = valor;
+    }
+    
+    private String valor;
+    
+    private Perfil(String valor) {
+        this.valor = valor;
+    }
+    
+    @Override
+    public String toString() {
+        return this.valor;
+    }
+}
