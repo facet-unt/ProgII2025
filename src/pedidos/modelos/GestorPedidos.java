2ffa46eb5c0ddd9f89c51d4477b3343697ff7341
Ssyhakar Natanael Josue <115747734+NatSsy@users.noreply.github.com>
2025-11-11 16:53:37 -0300

    corregir el metodo crear pedido

--- src/pedidos/modelos/GestorPedidos.java
+++ src/pedidos/modelos/GestorPedidos.java
@@ -4,7 +4,10 @@
  */
 package pedidos.modelos;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import usuarios.modelos.Cliente;
 
 /**
  *
@@ -15,10 +18,7 @@ public class GestorPedidos {
     private ArrayList<Pedido> pedidos = new ArrayList<>();
 
     private static GestorPedidos intstancia;
-
-    private GestorPedidos() {
-    }
-    
+    private int numnero;
     public static final String EXITO = "Pedido creado/modificado/cancelado con éxito";
     public static final String ERROR_FECHA = "La fecha del pedido es incorrecta";
     public static final String ERROR_HORA = "La hora del pedido es incorrecta";
@@ -28,5 +28,55 @@ public class GestorPedidos {
     public static final String ERROR_CANCELAR = "No se puede cancelar el pedido eneste estado";
     public static final String PEDIDOS_DUPLICADOS = "Ya existe un pedido con esehjnúmero";
     public static final String PEDIDO_INEXISTENTE = "No existe el pedido";
+
+    private GestorPedidos() {
+        
+    }
+
+    public String crearPedido(LocalDate fecha, LocalTime hora, ArrayList<ProductoDelPedido> productosDelPedido, Cliente cliente) {
+        if (fecha == null) {
+            return ERROR_FECHA;
+        }
+        if (hora == null) {
+            return ERROR_HORA;
+        }
+        if (productosDelPedido.isEmpty()){
+            return ERROR_PRODUCTOS_DEL_PEDIDO;
+        }
+        if (cliente == null) {
+            return ERROR_CLIENTE;      
+        }
+        Pedido nuevoPedido = new Pedido(fecha, hora,productosDelPedido,  cliente);
+        return null;
+    }
 }
+
+//public String crearPedido(LocalDate fecha, LocalTime hora,
+//ArrayList<ProductoDelPedido> productosDelPedido, Cliente cliente):
+//crea un nuevo pedido, devolviendo el resultado de la operación. Al
+//crear el pedido, el mismo se agrega al cliente especificado (empleando
+//Trabajo Práctico 5 - Programación II - 2025 3 | 5
+//
+//FACULTAD DE CIENCIAS EXACTAS Y TECNOLOGÍA (UNT) PROGRAMACIÓN II (E11)
+//
+//el método agregarPedido() definido en la clase Cliente).
+//○ public String cambiarEstado(Pedido pedidoAModificar): cambia el
+//estado de un pedido. Si el pedido está en el estado Estado.CREADO lo
+//pasa a Estado.PROCESANDO. Si el pedido está en el estado
+//Estado.PROCESANDO lo pasa a Estado.ENTREGADO. Devuelve el resultado de
+//la operación.
+//○ public ArrayList<Pedido> verPedidos(): devuelve todos los pedidos.
+//○ public boolean hayPedidosConEsteCliente(Cliente cliente): devuelve
+//true si hay al menos un pedido con el cliente especificado, false en
+//caso contrario.
+//○ public boolean hayPedidosConEsteProducto(Producto producto):
+//devuelve true si hay al menos un pedido con el producto especificado,
+//false en caso contrario.
+//○ public boolean existeEstePedido(Pedido pedido): devuelve true si
+//existe el pedido especificado, false en caso contrario.
+//○ public Pedido obtenerPedido(Integer numero): obtiene el pedido con
+//el número especificado. Si no hay un pedido con el número, devuelve
+//null.
+//○ Métodos auxiliares que considere necesarios (por ejemplo, los que
+//validen los datos de un pedido).
 
