aa54d09dc7e3a5d34e9f41f6fc6a4b88fa9a8846
Ssyhakar Natanael Josue <115747734+NatSsy@users.noreply.github.com>
2025-11-11 10:55:57 -0300

    Se creo el enum Perfil.java en usuarios.modelos

---
+++ src/usuarios/modelos/Perfil.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package usuarios.modelos;
+
+/**
+ *
+ * @author Amadeo
+ */
+public enum Perfil {
+    CLIENTE("Cliente"),
+    EMPLEADO("Empleado"),
+    ENCARGADO("Encargado");
+
+    public String verValor() {
+        return valor;
+    }
+
+    public void asignarValor(String valor) {
+        this.valor = valor;
+    }
+
+    private String valor;
+
+    private Perfil(String valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return this.valor;
+    }
+
+}
