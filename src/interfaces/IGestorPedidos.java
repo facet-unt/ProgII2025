ba3b2ab7848a4068e23f032dcc68e7b1361c5eca
Ssyhakar Natanael Josue <115747734+NatSsy@users.noreply.github.com>
2025-11-14 12:44:49 -0300

    Se creo IGestorPedidos(), IGestorUsuario()

---
+++ src/interfaces/IGestorPedidos.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfaces;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import pedidos.modelos.Pedido;
+import pedidos.modelos.ProductoDelPedido;
+import productos.modelos.Producto;
+import usuarios.modelos.Cliente;
+
+/**
+ *
+ * @author Lucas
+ */
+public interface IGestorPedidos {
+
+    public static final String EXITO = "Pedido creado/modificado/cancelado con éxito";
+    public static final String ERROR_FECHA = "La fecha del pedido es incorrecta";
+    public static final String ERROR_HORA = "La hora del pedido es incorrecta";
+    public static final String ERROR_PRODUCTOS_DEL_PEDIDO = "El pedido no tieneproductos";
+    public static final String ERROR_CLIENTE = "El pedido no tiene un cliente";
+    public static final String ERROR_ESTADO = "El pedido no tiene un estado";
+    public static final String ERROR_CANCELAR = "No se puede cancelar el pedido eneste estado";
+    public static final String PEDIDOS_DUPLICADOS = "Ya existe un pedido con esehjnúmero";
+    public static final String PEDIDO_INEXISTENTE = "No existe el pedido";
+
+    public String crearPedido(LocalDate fecha, LocalTime hora,
+            ArrayList<ProductoDelPedido> productosDelPedido, Cliente cliente);
+
+    public String cambiarEstado(Pedido pedidoAModificar);
+
+    public ArrayList<Pedido> verPedidos();
+
+    public boolean hayPedidosConEsteCliente(Cliente cliente);
+
+    public boolean hayPedidosConEsteProducto(Producto producto);
+
+    public String cancelarPedido(Pedido pedido);
+
+    public boolean existeEstePedido(Pedido pedido);
+
+    public Pedido obtenerPedido(Integer numero);
+}
---
+++ src/interfaces/IGestorUsuarios.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfaces;
+
+import java.util.ArrayList;
+import usuarios.modelos.Perfil;
+import usuarios.modelos.Usuario;
+
+/**
+ *
+ * @author Lucas
+ */
+public interface IGestorUsuarios {
+
+    public static final String EXITO = "Usuario creado/modificado con éxito";
+    public static final String ERROR_CORREO = "El correo del usuario es incorrecto";
+    public static final String ERROR_APELLIDO = "El apellido del usuario esincorrecto";
+    public static final String ERROR_NOMBRE = "El nombre del usuario es incorrecto";
+    public static final String ERROR_CLAVES = "Las claves especificadas no coinciden oson incorrectas";
+    public static final String ERROR_PERFIL = "El perfil del usuario es incorrecto";
+    public static final String USUARIOS_DUPLICADOS = "Ya existe un usuario con esecorreo";
+    public static final String VALIDACION_EXITO = "Los datos del usuario soncorrectos";
+
+    public String crearUsuario(String correo, String apellido, String nombre, Perfil perfil, String clave, String claveRepetida);
+    public ArrayList<Usuario> verUsuarios();
+
+    public ArrayList<Usuario> buscarUsuarios(String apellido);
+
+    public String borrarUsuario(Usuario usuario);
+
+    public boolean existeEsteUsuario(Usuario usuario);
+
+    public Usuario obtenerUsuario(String correo);
+}
