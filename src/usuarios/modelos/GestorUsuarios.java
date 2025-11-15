28a5b5a3d76c5d120b8b6b14104f6e7d7fd7f152
Gaston Romano <romanogaston13@gmail.com>
2025-11-15 11:50:09 -0300

    Implemento IGestorUsuarios

--- src/usuarios/modelos/GestorUsuarios.java
+++ src/usuarios/modelos/GestorUsuarios.java
@@ -4,23 +4,15 @@
  */
 package usuarios.modelos;
 
+import interfaces.IGestorUsuarios;
 import java.util.ArrayList;
+import pedidos.modelos.GestorPedidos;
 
 /**
  *
  * @author Gaston
  */
-public class GestorUsuarios {
-    public static final String EXITO = "Usuario creado/modificado con éxito";
-    public static final String ERROR_CORREO = "El correo del usuario es incorrecto";
-    public static final String ERROR_APELLIDO = "El apellido del usuario es incorrecto";
-    public static final String ERROR_NOMBRE = "El nombre del usuario es incorrecto";
-    public static final String ERROR_CLAVES = "Las claves especificadas no coinciden o son incorrectas";
-    public static final String ERROR_CLAVE = "La clave especificada no es valida";
-    public static final String ERROR_PERFIL = "El perfil del usuario es incorrecto";
-    public static final String USUARIOS_DUPLICADOS = "Ya existe un usuario con ese correo";
-    public static final String VALIDACION_EXITO = "Los datos del usuario son correctos";
-    
+public class GestorUsuarios implements IGestorUsuarios{
     private ArrayList<Usuario> usuarios = new ArrayList<>();
     
     private static GestorUsuarios instancia;
@@ -31,26 +23,41 @@ public class GestorUsuarios {
             instancia = new GestorUsuarios();
         return instancia;
     }
+    @Override
     public String crearUsuario(String correo, String apellido, String nombre, Perfil perfil, String clave, String claveRepetida){
-        String validacion = validarDatos(correo, apellido, nombre, clave, claveRepetida);
+        String validacion = validarDatos(correo.toLowerCase(), apellido, nombre, clave, claveRepetida);
         if(!validacion.equals(VALIDACION_EXITO)) return validacion;
-        Usuario u;
+        if(obtenerUsuario(correo) != null) return USUARIOS_DUPLICADOS;
+        Usuario nuevoUsuario;
+        switch (perfil) {
+        case CLIENTE -> nuevoUsuario = new Cliente(correo, apellido, nombre, clave);
+        case ENCARGADO -> nuevoUsuario = new Encargado(correo, apellido, nombre, clave);
+        case EMPLEADO -> nuevoUsuario = new Empleado(correo, apellido, nombre, clave);
+        default -> {
+            return ERROR_PERFIL;
+            }
+        }
+        usuarios.add(nuevoUsuario);
         return EXITO;
     }
+    @Override
     public ArrayList<Usuario> verUsuarios(){
         return usuarios;
     }
+    @Override
     public ArrayList<Usuario> buscarUsuarios(String apellido){
         ArrayList<Usuario> usuariosEncontrados = new ArrayList<>();
         for(Usuario u : usuarios)
             if(u.verApellido().equals(apellido.toLowerCase())) usuariosEncontrados.add(u);
         return usuariosEncontrados;
     }
+    @Override
     public boolean existeEsteUsuario(Usuario usuario){
         for(Usuario u : usuarios)
             if(u.equals(usuario)) return true;
         return false;
     }
+    @Override
     public Usuario obtenerUsuario(String correo){
         for(Usuario u : usuarios)
             if(u.verCorreo().equals(correo.toLowerCase())) return u;
@@ -62,8 +69,17 @@ public class GestorUsuarios {
         if(nombre == null || nombre.isBlank()) return ERROR_NOMBRE;
         if(clave == null || clave.isBlank()) return ERROR_CLAVE;
         if((claveRepetida == null || claveRepetida.isBlank()) || !claveRepetida.equals(clave)) return ERROR_CLAVES;
-        
         return VALIDACION_EXITO;
     }
-    
+    @Override
+    public String borrarUsuario(Usuario usuario){
+        if(usuario instanceof Cliente clienteABorrar){
+            GestorPedidos gp = GestorPedidos.instanciar();
+            if(gp.hayPedidosConEsteCliente(clienteABorrar))
+                return "Este Cliente tiene un pedido, no se puede borrar";
+        }
+        if(usuarios.remove(usuario)){
+            return "Usuario borrado con exito";
+        } else return "El usuario no esta";
+    }
 }
