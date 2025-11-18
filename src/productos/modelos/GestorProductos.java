f10a6c6b4036a0de20c7c6638e99763581c539df
Irina Caramuti <iricaramuti@gmail.com>
2025-11-18 17:00:56 -0300

    fix imports

--- src/productos/modelos/GestorProductos.java
+++ src/productos/modelos/GestorProductos.java
@@ -8,8 +8,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import pedidos.modelos.*;
 
 
