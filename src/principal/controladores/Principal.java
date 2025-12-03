8a175fb1503dbbd9dfb5d494eb994129acaabfb0
Rocio Ortega <rociortteg@gmail.com>
2025-12-02 23:28:55 -0300

    Agregado de las interfaces IControladorPrincipal, IControladorAMUsuario,  IControladorUsuarios, IControladorProductos e IControladorAMProducto. Creación de la clase Principal (main), ControladorPrincipal y VentanaPrincipal. Agregado de boton Usuarios, Productos, Pedidos y Salir. Boton salir cumple con su funcionamiento.

---
+++ src/principal/controladores/Principal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package principal.controladores;
+
+import interfaces.IControladorPrincipal;
+
+/**
+ *
+ * @author rocio
+ */
+public class Principal {
+    public static void main(String[] args) {
+         IControladorPrincipal controlador = new ControladorPrincipal();
+    }
+}
