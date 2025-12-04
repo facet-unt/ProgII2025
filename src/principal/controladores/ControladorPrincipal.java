03bb322f789532ebbff9fd623145ad019f0c6923
Norry Franco <norryianfranco@gmail.com>
2025-12-03 21:12:34 -0300

    Crear vista de ventana principal

--- src/principal/controladores/ControladorPrincipal.java
+++ src/principal/controladores/ControladorPrincipal.java
@@ -4,10 +4,38 @@
  */
 package principal.controladores;
 
+import interfaces.IControladorPrincipal;
+import java.awt.event.ActionEvent;
+import javax.swing.JOptionPane;
+import principal.vistas.VentanaPrincipal;
+
 /**
  *
  * @author damia
  */
-public class ControladorPrincipal {
-    
+public class ControladorPrincipal implements IControladorPrincipal{
+    static VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+    public static void main(String[] args) {
+        ventanaPrincipal.setLocationRelativeTo(null);
+        ventanaPrincipal.setTitle(TITULO);
+        ventanaPrincipal.setVisible(true);
+    }
+    @Override
+    public void btnProductosClic(ActionEvent evt) {
+        
+    }
+
+    @Override
+    public void btnUsuariosClic(ActionEvent evt) {
+        
+    }
+
+    @Override
+    public void btnSalirClic(ActionEvent evt) {
+        int opcion = JOptionPane.showConfirmDialog(ventanaPrincipal, "¿Esta seguro que quiere salir?");
+        if(opcion == JOptionPane.YES_OPTION){
+            ventanaPrincipal.dispose();
+            System.exit(0);
+        }
+    }
 }
