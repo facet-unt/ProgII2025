ab75201eea4c9ebef323dd9f483be46d3e5b98a0
Lazaro Mauricio Rojas <lazarojas.facet@gmail.com>
2025-12-05 09:50:55 -0300

    Crear controlador de VentanaProductos e implementar su visibilidad

---
+++ src/principal/controladores/ControladorVentanaProductos.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package principal.controladores;
+
+import interfaces.IControladorProductos;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
+import productos.vistas.VentanaProductos;
+
+/**
+ *
+ * @author lazar
+ */
+public class ControladorVentanaProductos implements IControladorProductos{
+    private VentanaProductos ventana;
+    
+    public ControladorVentanaProductos() {
+        this.ventana = new VentanaProductos(this);
+        this.ventana.setLocationRelativeTo(null);
+        this.ventana.setVisible(true);
+    }
+
+    @Override
+    public void ventanaObtenerFoco(WindowEvent evt) {
+    }
+
+    @Override
+    public void btnVolverClic(ActionEvent evt) {
+    }
+
+    @Override
+    public void btnBuscarClic(ActionEvent evt) {
+    }
+
+    @Override
+    public void txtDescripcionPresionarTecla(KeyEvent evt) {
+    }
+
+    @Override
+    public void btnNuevoClic(ActionEvent evt) {
+    }
+
+    @Override
+    public void btnModificarClic(ActionEvent evt) {
+    }
+
+    @Override
+    public void btnBorrarClic(ActionEvent evt) {
+    }
+}
