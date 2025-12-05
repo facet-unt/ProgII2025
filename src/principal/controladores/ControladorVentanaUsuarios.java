485b59521d3dedb67b65b5c959faff872e819327
Lazaro Mauricio Rojas <lazarojas.facet@gmail.com>
2025-12-05 09:49:32 -0300

    Crear controlador de VentanaUsuarios e implementar su visibilidad

---
+++ src/principal/controladores/ControladorVentanaUsuarios.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package principal.controladores;
+
+import interfaces.IControladorPrincipal;
+import interfaces.IControladorUsuarios;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
+import principal.vistas.VentanaPrincipal;
+import usuarios.vistas.VentanaUsuarios;
+
+/**
+ *
+ * @author lazar
+ */
+public class ControladorVentanaUsuarios implements IControladorUsuarios {
+    private VentanaUsuarios ventana;
+    
+    public ControladorVentanaUsuarios() {
+        this.ventana = new VentanaUsuarios(this);
+        this.ventana.setLocationRelativeTo(null);
+        this.ventana.setVisible(true);
+    }
+
+    @Override
+    public void btnNuevoClic(ActionEvent evt) {
+    }
+
+    @Override
+    public void btnModificarClic(ActionEvent evt) {
+    }
+
+    @Override
+    public void btnBorrarClic(ActionEvent evt) {
+    }
+
+    @Override
+    public void ventanaObtenerFoco(WindowEvent evt) {
+    }
+
+    @Override
+    public void btnVolverClic(ActionEvent evt) {
+    }
+
+    @Override
+    public void txtApellidoPresionarTecla(KeyEvent evt) {
+    }
+
+    @Override
+    public void btnBuscarClic(ActionEvent evt) {
+    }
+}
