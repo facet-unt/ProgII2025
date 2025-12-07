a3d32400927f88fdc9ab5af328f86f5d41dccb48
Agustin Arborio <agsuarborio@gmail.com>
2025-12-06 21:04:52 -0300

    agregado de mensajes en botones
    subrayado de mayusculas en botones
    
    creacion de la ventana de usuarios
    
    injtento de centrar pantalla de cliente
    
    prueba de metodos (falta controlar)

--- src/interfaces/IGestorProductos.java
+++ src/interfaces/IGestorProductos.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
 package Interfaces;
 
 import java.util.ArrayList;
@@ -30,12 +34,11 @@ import productos.modelos.Producto;
     public static final String VALIDACION_EXITO = "Los datos del producto son correctos";
     public static final String PRODUCTO_INEXISTENTE = "No existe el producto especificado";
     public static final String ERROR_BORRADO = "Hay un pedido con este producto";
-    public static final String EXITO_BORRADO = "se borro el producto con exito";
-    
-    public static final String LECTURA_ERROR = "Error al leer los productos";
-    public static final String CREACION_ERROR = "Error al crear el archivo de productos";
-    public static final String LECTURA_OK = "Se pudieron leer los productos";
-    public static final String CREACION_OK = "Se pudo crear el archivo de productos";
-    public static final String ESCRITURA_OK = "Se pudieron guardar los productos";
-    public static final String ESCRITURA_ERROR = "Error al guardar los productos";
+    public static final String EXITO_BORRADO = "Se borro el producto con exito";
+    public static final String ESCRITURA_ERROR = "Error de escritura";
+    public static final String CREACION_ERROR = "Error de creacion";
+    public static final String CREACION_OK = "Se creo exitosamente";
+    public static final String ESCRITURA_OK = "Se escribio correctamente";
+    public static final String LECTURA_OK = "Se leyo correctramente";
+    public static final String LECTURA_ERROR = "Error al leer";
 }
