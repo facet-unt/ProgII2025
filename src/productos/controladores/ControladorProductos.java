e16d5b69ee4105af63d3e38fde7132fa46e75a30
Lucas <90874300+vvintra@users.noreply.github.com>
2025-12-07 16:22:58 -0300

    Corecta Ubicacion de los controladores

--- src/productos/controladores/ControladorProductos.java
+++ src/productos/controladores/ControladorProductos.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package principal.controladores;
+package productos.controladores;
 import interfaces.IControladorProductos;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
